9f5082a6095feb9bff9583bbeb3eae6dc8b58f03
zhangxueying <ying@qq.com>
2019-01-14 18:39:24 +0800

    添删显示

--- main.c
+++ main.c
@@ -10,12 +10,47 @@ int main(int argc, char *argv[])
     {
         printf("1--tianjiayigeminganzifu\n");
         printf("2--shanchuzuihouyigeminganzifu\n");
-        printf("3---tihuansuoyouzifu\n");
-        printf("4--xianshisuoyoum9nganzifu\n");
-        printf("5--tuichu\n");
+        printf("3---xianshisuoyoum9nganzifu\n");
         printf("qingshuruxiangyingdegongnengbiaohao\n");
         scanf("%d", &code);
+        if (code == 1)
+        {
+            char a;
+            char x;
+            printf("qingshuruyaochucundezifu\n");
+            scanf("%c", &a);
+            scanf("%c", &a);
+            arr[index] = a;
+            index += 1;
+            printf("tianjiachengong,dianjihuichejixu\n");
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
+        if (code == 2)
+        {
+            char x;
+            printf("shanchuzuihouyigeminganzifu");
+            index -= 1;
+            printf("shanchuchenggong,dianjihuichejixu");
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
 
-        return 0;
+        if (code == 3)
+        {
+            char x;
+            int i = 0;
+            printf("xianshisuoyoudeminganzifu\n");
+            for (; i < index; i++)
+            {
+                printf("%c\n", arr[i]);
+            }
+            printf("dianjihuichejixu\n");
+            scanf("%c", &x);
+            scanf("%c", &x);
+        }
     }
+
+    return 0;
 }
+
