15b1c036a683547d17a5e8f7116c284112f52e7a
zhangxueying <ying@qq.com>
2019-01-14 17:26:58 +0800

    开始

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[])
+{
+    char arr[1000];
+    int index = 0;
+    int code = 0;
+    printf("jinruminganzifuguanlixitong\n");
+    while (1)
+    {
+        printf("1--tianjiayigeminganzifu\n");
+        printf("2--shanchuzuihouyigeminganzifu\n");
+        printf("3---tihuansuoyouzifu\n");
+        printf("4--xianshisuoyoum9nganzifu\n");
+        printf("5--tuichu\n");
+        printf("qingshuruxiangyingdegongnengbiaohao\n");
+        scanf("%d", &code);
 
-    printf("hello world!");
-
-    return 0;
-
+        return 0;
+    }
 }
